73046c8abff4407917458627223a75711fc91baa
de-meyer <dmeyer.private@gmail.com>
2025-02-17 23:44:36 +0100

    cleanup

--- src/main/kotlin/com/cli/fancy/broadcastserver/WebSocketConfig.kt
+++ src/main/kotlin/com/cli/fancy/broadcastserver/WebSocketConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.session.Session
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 
 @Configuration
