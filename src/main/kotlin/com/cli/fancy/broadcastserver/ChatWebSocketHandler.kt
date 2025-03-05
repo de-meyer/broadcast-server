34ba105c1e6a30d0ae9b9281f8090cba0bbd3317
de-meyer <dmeyer.private@gmail.com>
2025-03-05 23:48:48 +0100

    add info of connections

--- src/main/kotlin/com/cli/fancy/broadcastserver/ChatWebSocketHandler.kt
+++ src/main/kotlin/com/cli/fancy/broadcastserver/ChatWebSocketHandler.kt
@@ -5,14 +5,18 @@ import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.WebSocketSession
+import java.util.function.Consumer
 
 @Component
 class ChatWebSocketHandler : WebSocketHandler {
     private val sessions = mutableSetOf<WebSocketSession>()
-
+    private val clients = mutableListOf<String>()
     override fun afterConnectionEstablished(session: WebSocketSession) {
         sessions.add(session)
+        clients.add(session.id)
         session.sendMessage(TextMessage("Connected to WebSocket Server"))
+        sessions.forEach { it.sendMessage(TextMessage("New client connected: ${session.id}")) }
+        println("Clients connected to server: ${clients.forEach{println(it)}}")
     }
 
     override fun handleMessage(session: WebSocketSession, message: org.springframework.web.socket.WebSocketMessage<*>) {
@@ -28,8 +32,14 @@ class ChatWebSocketHandler : WebSocketHandler {
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
         sessions.remove(session)
+        clients.remove(session.id)
+        sessions.forEach { it.sendMessage(TextMessage("Client disconnected: ${session.id}")) }
         println("Client disconnected")
     }
 
     override fun supportsPartialMessages(): Boolean = false
+
+    fun getConnectedClients(): List<String> {
+        return clients
+    }
 }
